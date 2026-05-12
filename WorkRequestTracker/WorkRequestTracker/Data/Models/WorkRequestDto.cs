01878267cff01b5ed9294559fc7a1df08d5fc25a
tarunvd <tarunvd@gmail.com>
2026-05-12 20:24:48 +0530

    api layer with sql complete

--- WorkRequestTracker/WorkRequestTracker/Data/Models/WorkRequestDto.cs
+++ WorkRequestTracker/WorkRequestTracker/Data/Models/WorkRequestDto.cs
@@ -4,17 +4,17 @@ namespace WorkRequestTracker.Data.Models
 {
     public class WorkRequestDto
     {
-        public required string Title { get; set; }
+        public string? Title { get; set; }
 
-        public required string ClientName { get; set; }
+        public string? ClientName { get; set; }
 
-        public required string Description { get; set; }
+        public string? Description { get; set; }
 
-        public required PriorityEnum Priority { get; set; }
+        public PriorityEnum? Priority { get; set; }
         
-        public required StatusEnum Status { get; set; }
+        public StatusEnum? Status { get; set; }
 
-        public required DateTime DueDate { get; set; }
+        public DateTime? DueDate { get; set; }
 
         public string? Notes { get; set; }
     }
